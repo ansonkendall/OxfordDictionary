f146d38f3a825f7f05cf814d7ca4200837b285f9
kjergens <kjergens@users.noreply.github.com>
2018-02-15 13:47:29 -0500

    idk

---
+++ src/test/PasswordMaker.java
@@ -0,0 +1,28 @@
+package test;
+
+/**
+YOU FAIL!
+**/
+public class PasswordMaker {
+
+    public static void main(String[] args) {
+        // Use the Dictionary library to create a secure password.
+        // ADDING A COMMENT
+
+        // 1. Get 2 random words and put them together.
+        // 1a. Make sure it is at least 8 characters long. If not, do it again.
+
+        // 2. Replace the vowels with numbers following these rules:
+        //    a becomes 4
+        //    e becomes 3
+        //    i becomes 1
+        //    0 becomes 0
+        //    u becomes 6
+
+        // 3. Capitalize the first letter
+
+        // 4. Tell the person their password.
+
+        // BONUS 5. Ask the person if they want another one and make another one.
+    }
+}
